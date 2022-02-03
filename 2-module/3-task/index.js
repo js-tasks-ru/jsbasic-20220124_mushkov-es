4c63e9105dfe599c1c8c6a671c81e2917638c0f2
Eugene Mushkov <mushkov_es@enm.spb.ru>
2022-02-03 22:31:39 +0300

    added context this in 2-module 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,15 +1,17 @@
 let calculator = {
   read: function(a, b) {
-      calculator.firstValue = a;
-      calculator.secondValue = b;
+      this.firstValue = a;
+      this.secondValue = b;
   },
   sum() {
-      return calculator.firstValue + calculator.secondValue;
+      return this.firstValue + calculator.secondValue;
   },
   mul: function() {
-      return calculator.firstValue * calculator.secondValue
+      return this.firstValue * calculator.secondValue
   },
 };
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
